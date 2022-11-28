ccac4e7b7b7978d6cc01e3ab585f620f7ed24b80
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-28 11:32:02 +0530

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewFriendComponent } from './view-friend/view-friend.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const myroute:Routes=[
   {
@@ -35,7 +36,8 @@ const myroute:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myroute)
+    RouterModule.forRoot(myroute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
